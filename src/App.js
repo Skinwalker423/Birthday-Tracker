c10643d72c87cd0645a1aa65b8616266643b38ee
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-07-29 20:13:23 -0700

    birthday list card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+
 function App() {
+
+  const [people, setPeople] = useState(data);
 
   const handleClearAll = () =>{
     console.log('you clicked me');
+    setPeople([]);
   }
 
   return (
     <main>
       <section className='container'>
-        <h3>0 birthdays today</h3>
-        <List />
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
         <button onClick={handleClearAll}>clear all</button>
       </section>
     </main>
--- src/List.js
+++ src/List.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
+
+  const birthdayList = people.map(({name, age, id, image}) => {
+      return(
+        <div key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>Name: {name}</h4>
+              <p>Age: {age}</p>
+            </div>
+        </div>
+      )
+  })
+
   return (
     <>
-      <h2>list component</h2>
+      {birthdayList}
     </>
   );
 };
